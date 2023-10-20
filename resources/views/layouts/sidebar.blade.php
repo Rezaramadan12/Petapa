98419e64254bf276090ca2c049d6d50bd9b300b4
Rezaramadan221299 <rezaramadan224@gmail.com>
2023-10-20 15:47:59 +0700

    perubahan

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -47,7 +47,7 @@
                 </a>
             </li> --}}
             <li class="nav-item">
-                <a class="nav-link" href="/cobamaps">
+                <a class="nav-link" href="/maps">
                     <i class="fas fa-fw fa-map"></i>
                     <span>Peta Tempat Sampah</span>
                 </a>
