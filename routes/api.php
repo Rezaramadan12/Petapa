1336adc4d5f5edafda8bcfa3da0a5110d104cdf8
Rezaramadan221299 <rezaramadan224@gmail.com>
2023-11-15 15:34:05 +0700

    merapikan file

--- routes/api.php
+++ routes/api.php
@@ -6,11 +6,7 @@ use App\Http\Controllers\UntanController;
 use App\Http\Controllers\API\untansController;
 use App\Http\Controllers\API\polnepsController;
 use App\Http\Controllers\API\rusunawasController;
-use App\Http\Controllers\API\umpsController;
 use App\Http\Controllers\API\SiskomController;
-use App\Http\Controllers\API\UserController;
-use App\Http\Controllers\API\ProductController;
-use App\Http\Controllers\dummyAPI;
 
 /*
 |--------------------------------------------------------------------------
@@ -28,27 +24,9 @@ Route::post('polnep',[polnepsController::class,'postedgepolnep']);
 Route::get('polnep',[polnepsController::class,'postedgepolnep']);
 Route::post('rusunawa',[rusunawasController::class,'postedgerusunawa']);
 Route::get('rusunawa',[rusunawasController::class,'postedgerusunawa']);
-Route::post('ump',[umpsController::class,'postedgeump']);
-Route::get('ump',[umpsController::class,'postedgeump']);
 Route::post('siskom',[SiskomController::class,'getedgesiskom']);
 Route::get('siskom',[SiskomController::class,'getedgesiskom']);
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-
-// Route::get("data",[dummyAPI::class,'getData']);
-
-// Route::middleware('auth:api')->group(function(){
-//     // Rute yang membutuhkan autentikasi
-
-//     Route::get('/user', [UserController::class, 'getUser']);
-    Route::post('Untan', [UntanController::class, 'getUntan']);
-//     Route::put('/user/{id}', [UserController::class, 'updateUser']);
-//     Route::delete('/user/{id}', [UserController::class, 'deleteUser']);
-// });
-
-
-// // Rute yang tidak membutuhkan autentikasi
-
-// Route::get('/products', [ProductController::class, 'index']);
-// Route::get('/products/{id}', [ProductController::class, 'show']);
+Route::post('Untan', [UntanController::class, 'getUntan']);
--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MapsController;
-use App\Http\Controllers\RuteController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\API\untansController;
-use App\Http\Controllers\API\umpsController;
 use App\Http\Controllers\API\rusunawasController;
 use App\Http\Controllers\API\polnepsController;
 use App\Http\Controllers\API\SiskomController;
@@ -21,19 +19,12 @@ use App\Http\Controllers\API\SiskomController;
 |
 */
 
-// Route::get('/', function () {
-//     return 'test';
-// });
 
 Route::get('/', [DashboardController::class, 'index'])->name('dashboard.index');
 Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard.index');
 Route::get('maps', [MapsController::class, 'cobaindex'])->name('maps.cobaindex');
-// Route::get('cobamaps', [MapsController::class, 'cobaindex'])->name('maps.cobaindex');
 Route::get('data',[MapsController::class,'data'])->name('maps.data');
-Route::get('rutepengangkutan', [RuteController::class, 'index'])->name('rutepengangkutan.index');
-
 Route::get('/untan', [untansController::class, 'index'])->name('untan.index');
 Route::get('/polnep', [polnepsController::class, 'index'])->name('polnep.index');
-Route::get('/ump', [umpsController::class, 'index'])->name('ump.index');
 Route::get('/rusunawa', [rusunawasController::class, 'index'])->name('rusunawa.index');
 Route::get('/siskom', [SiskomController::class, 'index'])->name('siskom.index');
